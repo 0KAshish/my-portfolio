2829ad3e73720af893aef66dc5dc98a2bd0e7a75
Ashish Kumar <Ashishsam.ceh1997@gmail.com>
2025-10-16 10:17:30 +0530

    Add Contact and Footer components to Home page
    Rendered Contact and Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Hero from '@/components/Hero'
 import About from '@/components/About'
 import Projects from '@/components/Projects'
+import Contact from '@/components/Contact'
+import Footer from '@/components/Footer'
 
 export default function Home(){
   return(
@@ -8,6 +10,8 @@ export default function Home(){
       <Hero/>
       <About/>
       <Projects/>
+      <Contact/>
+      <Footer/>
     </div>
   )
 }
